d85f95edf5c1e693d39823cb5d9ac7098f787b25
TamLe2001 <TamLe2001@live.dk>
2019-06-12 11:07:16 +0200

    xd

--- Associationsopgave/screen.cpp
+++ Associationsopgave/screen.cpp
@@ -1,4 +1,5 @@
 #include "screen.h"
+#include <windows.h>
 
 Screen::Screen(string type)
 {
@@ -7,8 +8,13 @@ Screen::Screen(string type)
 
 void Screen::showApps(std::list<App*> apps)
 {
+/*    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    int k = 1;
+    SetConsoleTextAttribute(hConsole, k);*/
     std::cout << "On the " << type_ << " screen - Showing apps: " << endl;
     for(App* app : apps){
+        if(app->getName() == "Facebook")
         std::cout << "App with name " << app->getName() << endl;
         std::cout << "App with size " << app->getSize() << "MB" << endl;
         std::cout << "App with description " << app->getDescription() << endl << endl;
